7ef8e45f36c4a9f868cbde91b80811310f29e161
john-reiner <johnreiner1990@gmail.com>
2021-03-03 17:25:26 -0500

    fixed blogs links and projects show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_26_131822) do
+ActiveRecord::Schema.define(version: 2021_03_03_121700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_02_26_131822) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.text "summary"
     t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
