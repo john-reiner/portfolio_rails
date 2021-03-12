64cacf10fce4653642186117c7d05318c9c92806
john-reiner <johnreiner1990@gmail.com>
2021-03-12 08:41:32 -0500

    added tech_bio to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_133232) do
+ActiveRecord::Schema.define(version: 2021_03_12_133913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 2021_03_12_133232) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+    t.text "tech_bio"
   end
 
   add_foreign_key "blog_technologies", "blogs"
